f21a7940ce10999f5453ae9300302c16e8efca2b
lucas1004jx <lucas1004jx@gmail.com>
2017-08-31 00:14:08 +0200

    add animation for background omg

--- script/index.js
+++ script/index.js
@@ -75,23 +75,48 @@ $(function () {
 
 
     //----------------foto overlay------------------
-    
+
     function showImg() {
         $owl.find(".center img").click(function () {
+
+
             $("#foto-overlay").fadeIn();
             var srcImg = $(this).attr("src");
             $("#foto-overlay img").attr("src", srcImg);
-
+            imgResize();
         });
+
     }
 
-   
+    function imgResize() {
+        var wWidth = $(window).width();
+        var wHeight = $(window).height();
+        var imgWidth = $("#foto-overlay img").width();
+        var imgHeight = $("#foto-overlay img").height();
+        var ratio= imgWidth/imgHeight;
+        var wRatio = imgWidth/wWidth;
+        var hRation=imgHeight/wHeight;
+
+        if(wRatio>=hRation){ 
+          imgWidth= wWidth-50;
+           }else{
+             imgHeight= wHeight-50; 
+               imgWidth=imgHeight*ratio;
+           }
+      
+        
+        $("#foto-overlay img").css("width", imgWidth +"px");
+        $("#foto-overlay img").css("max-width", "100%");
+    }
+
     showImg();
+
+
 
     $(".aspa,#foto-overlay img").click(function () {
         $("#foto-overlay").fadeOut();
     });
-    
+
     //-------------tarifa -----------------
     /*$(".tarifa").mouseover(function () {
         var $this = $(this);
@@ -122,12 +147,12 @@ $(function () {
     }
 
     //------------scroll magic---------------
-   
-    
-    
+
+
+
     $("#intro .text").lettering("words");
-     $(".client .text").lettering("words");
-    
+    $(".client .text").lettering("words");
+
     var introTl = new TimelineLite();
 
 
@@ -156,8 +181,8 @@ $(function () {
             opacity: 0,
         }, 0.01);
 
-   
-   
+
+
 
 
     var toñoTl = new TimelineLite();
@@ -204,10 +229,10 @@ $(function () {
             y: 50
         });
 
-    
 
-    
-    
+
+
+
     var anaTl = new TimelineLite();
     anaTl.fromTo(["#woman", "#text-ana"], 0.5, {
             height: "0px",
@@ -230,7 +255,7 @@ $(function () {
             y: 50
         });
 
-   
+
 
     var teamTl = new TimelineLite();
     teamTl.from("#team h1", 0.5, {
@@ -266,7 +291,7 @@ $(function () {
                 }
             },
             opacity: 0
-        },0.1)
+        }, 0.1)
         .staggerFrom(".tarifa .text", 0.5, {
             cycle: {
                 x: function () {
@@ -277,276 +302,313 @@ $(function () {
                 }
             },
             opacity: 0
-        },0.1);
+        }, 0.1);
 
-    var galleryTl =new TimelineLite();
-    
-    galleryTl.from("#gallery header",1,{opacity:0,y:100});
-    
-     var contactTl = new TimelineLite();
-      contactTl.from("#contact h1",1,{opacity:0,y:100})
-    .from("address",1,{opacity:0,x:-200})
-    .from("#map",1,{opacity:0,scale:0});
-    
-    
-    
-     function scroll1024(){  
-         var controller = new ScrollMagic.Controller();
-   var scene1 = new ScrollMagic.Scene({
-            triggerElement: "#intro",
-            triggerHook: 0.5
-        }).setTween(introTl)
-        /*.addIndicators({
-            name: "intro",
-            colorTrigger: "black",
-            colorStart: "white",
-            colorEnd: "#fff"
-        })*/
-        .addTo(controller);
-    
-    var scene2 = new ScrollMagic.Scene({
-            triggerElement: "#team",
-            triggerHook: 0.4,
-            duration: "20%"
-        }).setTween(toñoTl)
-        /*.addIndicators({
-            name: "toño"
-        })*/
-        .addTo(controller);
+    var galleryTl = new TimelineLite();
 
-    var scene3 = new ScrollMagic.Scene({
-            triggerElement: "#team",
-            triggerHook: 0.2,
-            duration: "20%"
-        }).setTween(anaTl)
-        /* .addIndicators({
-         name:"ana"
-         })*/
-        .addTo(controller);
-
-    var scene4 = new ScrollMagic.Scene({
-            triggerElement: "#team",
-            triggerHook: 0.5,
-            duration: 100
-        }).setTween(teamTl)
-        /*.addIndicators({
-        name:"team header"
-        })*/
-        .addTo(controller);
-
-    var scene5 = new ScrollMagic.Scene({
-            triggerElement: "#intro .about",
-            triggerHook: 0.1,
-            duration: "88.4%"
-        }).setPin("#intro .about")
-        /*.addIndicators()*/
-        .addTo(controller);
-
-    var scene6= new ScrollMagic.Scene({
-        triggerElement:"#tarifa",
-        triggerHook:0.5,
-        duration:"50%"
-    })
-    .setTween(tarifaTl)
-    /*.addIndicators()*/
-    .addTo(controller);
-    
-    var scene7= new ScrollMagic.Scene({
-        triggerElement:"#gallery",
-        triggerHook:1
-    })
-    .setTween(galleryTl)
-    /*.addIndicators({
-        name:"gallery"
-    })*/
-    .addTo(controller);
-    
-    var scene8= new ScrollMagic.Scene({
-        triggerElement:"#client",
-        triggerHook:0.5,
-        duration:"50%"
-    })
-    .setTween(TweenMax.from("#client h1",1,{opacity:0,y:100}))
-    /*.addIndicators()*/
-    .addTo(controller);
-         
-         $(".client").each(function(){
-             var tl= new TimelineLite();
-            
-             tl.from($(this).find("figure"),1,{opacity:0,scale:0})
-                .from($(this).find("h4"),1,{opacity:0,x:200})
-                .staggerFrom($(this).find("p span"),1,{
-                 cycle: {
-                x: function () {
-                    return Math.random() * 600 - 300
-                },
-                y: function () {
-                    return Math.random() * 600 - 300
-                }
-            },
-            opacity: 0,
-             },0.1);
-             
-            var scene8= new ScrollMagic.Scene({
-                triggerElement:this,
-                triggerHook:0.5,
-                duration:"20%"
-            }).setTween(tl)
-            /*.addIndicators()*/
-            .addTo(controller); 
-         });
-         
-         
-         var scene9= new ScrollMagic.Scene({
-        triggerElement:"#contact",
-        triggerHook:0.5,
-        duration:"50%"
-    })
-    .setTween(contactTl)
-    /*.addIndicators()*/
-    .addTo(controller);
-         
-     }//------scroll1024
-    
-     function scrollMovil(){  
-
-         
-         
-         var controller = new ScrollMagic.Controller();
-   var scene1 = new ScrollMagic.Scene({
-            triggerElement: "#intro",
-            triggerHook:1
-        }).setTween(introTl)
-       /* .addIndicators({
-            name: "intro",
-            colorTrigger: "black",
-            colorStart: "white",
-            colorEnd: "#fff"
-        })*/
-        .addTo(controller);
-    
-    var scene2 = new ScrollMagic.Scene({
-            triggerElement: "#team",
-            triggerHook: 0.4,
-            
-        }).setTween(toñoTl)
-        /*.addIndicators({
-            name: "toño"
-        })*/
-        .addTo(controller);
-
-    var scene3 = new ScrollMagic.Scene({
-            triggerElement: "#team",
-            triggerHook: 0.1,
-            
-        }).setTween(anaTl)
-        /* .addIndicators({
-         name:"ana"
-         })*/
-        .addTo(controller);
-
-    var scene4 = new ScrollMagic.Scene({
-            triggerElement: "#team",
-            triggerHook: 0.5,
-            
-        }).setTween(teamTl)
-        /*.addIndicators({
-        name:"team header"
-        })*/
-        .addTo(controller);
-
-   
-
-    var scene6= new ScrollMagic.Scene({
-        triggerElement:"#tarifa",
-        triggerHook:0.5
-    })
-    .setTween(tarifaTl)
-    /*.addIndicators()*/
-    .addTo(controller);
-    
-    var scene7= new ScrollMagic.Scene({
-        triggerElement:"#gallery",
-        triggerHook:1
-    })
-    .setTween(galleryTl)
-    /*.addIndicators()*/
-    .addTo(controller);
-    
-    var scene8= new ScrollMagic.Scene({
-        triggerElement:"#client",
-        triggerHook:0.5,
-        duration:"50%"
-    })
-    .setTween(TweenMax.from("#client h1",1,{opacity:0,y:100}))
-    /*.addIndicators()*/
-    .addTo(controller);
-         
-         $(".client").each(function(){
-             var tl= new TimelineLite();
-            
-             tl.from($(this).find("figure"),1,{opacity:0,scale:0})
-                .from($(this).find("h4"),1,{opacity:0,x:200})
-                .staggerFrom($(this).find("p span"),1,{
-                 cycle: {
-                x: function () {
-                    return Math.random() * 600 - 300
-                },
-                y: function () {
-                    return Math.random() * 600 - 300
-                }
-            },
-            opacity: 0,
-             },0.1);
-             
-            var scene8= new ScrollMagic.Scene({
-                triggerElement:this,
-                triggerHook:0.5,
-                duration:"20%"
-            }).setTween(tl)
-            /*.addIndicators()*/
-            .addTo(controller); 
-         });
-         
-         
-         var scene9= new ScrollMagic.Scene({
-        triggerElement:"#contact",
-        triggerHook:0.5,
-        duration:"50%"
-    })
-    .setTween(contactTl)
-    /*.addIndicators()*/
-    .addTo(controller);
-         
-     }//------scrollmovil
-    
-    if($(window).width()>=1024){
-      scroll1024();
-       }else{
-           scrollMovil();
-       }
-    
-  var wWidth=$(window).width();
-var screen="";
-    if(wWidth<1024){
-        screen="small";
-    }else if(wWidth>1024){
-          screen="big";   
-             }
-    
-    $(window).resize(function(){
-        var currentWidth= $(window).width();
-        
-        if(currentWidth<1024 && screen==="big"){
-            location.reload();
-            screen="small";
-            console.log("small");
-        }else if(currentWidth>=1024 && screen==="small"){
-             location.reload();
-            screen="big";
-            console.log("big");
-                 }
+    galleryTl.from("#gallery header", 1, {
+        opacity: 0,
+        y: 100
     });
-    
 
+    var contactTl = new TimelineLite();
+    contactTl.from("#contact h1", 1, {
+            opacity: 0,
+            y: 100
+        })
+        .from("address", 1, {
+            opacity: 0,
+            x: -200
+        })
+        .from("#map", 1, {
+            opacity: 0,
+            scale: 0
+        });
+
+
+
+    function scroll1024() {
+        var controller = new ScrollMagic.Controller();
+        var scene1 = new ScrollMagic.Scene({
+                triggerElement: "#intro",
+                triggerHook: 0.5
+            }).setTween(introTl)
+            /*.addIndicators({
+                name: "intro",
+                colorTrigger: "black",
+                colorStart: "white",
+                colorEnd: "#fff"
+            })*/
+            .addTo(controller);
+
+        var scene2 = new ScrollMagic.Scene({
+                triggerElement: "#team",
+                triggerHook: 0.4,
+                duration: "20%"
+            }).setTween(toñoTl)
+            /*.addIndicators({
+                name: "toño"
+            })*/
+            .addTo(controller);
+
+        var scene3 = new ScrollMagic.Scene({
+                triggerElement: "#team",
+                triggerHook: 0.2,
+                duration: "20%"
+            }).setTween(anaTl)
+            /* .addIndicators({
+             name:"ana"
+             })*/
+            .addTo(controller);
+
+        var scene4 = new ScrollMagic.Scene({
+                triggerElement: "#team",
+                triggerHook: 0.5,
+                duration: 100
+            }).setTween(teamTl)
+            /*.addIndicators({
+            name:"team header"
+            })*/
+            .addTo(controller);
+
+        var scene5 = new ScrollMagic.Scene({
+                triggerElement: "#intro .about",
+                triggerHook: 0.1,
+                duration: "88.4%"
+            }).setPin("#intro .about")
+            /*.addIndicators()*/
+            .addTo(controller);
+
+        var scene6 = new ScrollMagic.Scene({
+                triggerElement: "#tarifa",
+                triggerHook: 0.5,
+                duration: "50%"
+            })
+            .setTween(tarifaTl)
+            /*.addIndicators()*/
+            .addTo(controller);
+
+        var scene7 = new ScrollMagic.Scene({
+                triggerElement: "#gallery",
+                triggerHook: 1
+            })
+            .setTween(galleryTl)
+            /*.addIndicators({
+                name:"gallery"
+            })*/
+            .addTo(controller);
+
+        var scene8 = new ScrollMagic.Scene({
+                triggerElement: "#client",
+                triggerHook: 0.5,
+                duration: "50%"
+            })
+            .setTween(TweenMax.from("#client h1", 1, {
+                opacity: 0,
+                y: 100
+            }))
+            /*.addIndicators()*/
+            .addTo(controller);
+
+        $(".client").each(function () {
+            var tl = new TimelineLite();
+
+            tl.from($(this).find("figure"), 1, {
+                    opacity: 0,
+                    scale: 0
+                })
+                .from($(this).find("h4"), 1, {
+                    opacity: 0,
+                    x: 200
+                })
+                .staggerFrom($(this).find("p span"), 1, {
+                    cycle: {
+                        x: function () {
+                            return Math.random() * 600 - 300
+                        },
+                        y: function () {
+                            return Math.random() * 600 - 300
+                        }
+                    },
+                    opacity: 0,
+                }, 0.1);
+
+            var scene8 = new ScrollMagic.Scene({
+                    triggerElement: this,
+                    triggerHook: 0.5,
+                    duration: "20%"
+                }).setTween(tl)
+                /*.addIndicators()*/
+                .addTo(controller);
+        });
+
+
+        var scene9 = new ScrollMagic.Scene({
+                triggerElement: "#contact",
+                triggerHook: 0.5,
+                duration: "50%"
+            })
+            .setTween(contactTl)
+            /*.addIndicators()*/
+            .addTo(controller);
+
+    } //------scroll1024
+
+    function scrollMovil() {
+
+
+
+        var controller = new ScrollMagic.Controller();
+        var scene1 = new ScrollMagic.Scene({
+                triggerElement: "#intro",
+                triggerHook: 1
+            }).setTween(introTl)
+            /* .addIndicators({
+                 name: "intro",
+                 colorTrigger: "black",
+                 colorStart: "white",
+                 colorEnd: "#fff"
+             })*/
+            .addTo(controller);
+
+        var scene2 = new ScrollMagic.Scene({
+                triggerElement: "#team",
+                triggerHook: 0.4,
+
+            }).setTween(toñoTl)
+            /*.addIndicators({
+                name: "toño"
+            })*/
+            .addTo(controller);
+
+        var scene3 = new ScrollMagic.Scene({
+                triggerElement: "#team",
+                triggerHook: 0.1,
+
+            }).setTween(anaTl)
+            /* .addIndicators({
+             name:"ana"
+             })*/
+            .addTo(controller);
+
+        var scene4 = new ScrollMagic.Scene({
+                triggerElement: "#team",
+                triggerHook: 0.5,
+
+            }).setTween(teamTl)
+            /*.addIndicators({
+            name:"team header"
+            })*/
+            .addTo(controller);
+
+
+
+        var scene6 = new ScrollMagic.Scene({
+                triggerElement: "#tarifa",
+                triggerHook: 0.5
+            })
+            .setTween(tarifaTl)
+            /*.addIndicators()*/
+            .addTo(controller);
+
+        var scene7 = new ScrollMagic.Scene({
+                triggerElement: "#gallery",
+                triggerHook: 1
+            })
+            .setTween(galleryTl)
+            /*.addIndicators()*/
+            .addTo(controller);
+
+        var scene8 = new ScrollMagic.Scene({
+                triggerElement: "#client",
+                triggerHook: 0.5,
+                duration: "50%"
+            })
+            .setTween(TweenMax.from("#client h1", 1, {
+                opacity: 0,
+                y: 100
+            }))
+            /*.addIndicators()*/
+            .addTo(controller);
+
+        $(".client").each(function () {
+            var tl = new TimelineLite();
+
+            tl.from($(this).find("figure"), 1, {
+                    opacity: 0,
+                    scale: 0
+                })
+                .from($(this).find("h4"), 1, {
+                    opacity: 0,
+                    x: 200
+                })
+                .staggerFrom($(this).find("p span"), 1, {
+                    cycle: {
+                        x: function () {
+                            return Math.random() * 600 - 300
+                        },
+                        y: function () {
+                            return Math.random() * 600 - 300
+                        }
+                    },
+                    opacity: 0,
+                }, 0.1);
+
+            var scene8 = new ScrollMagic.Scene({
+                    triggerElement: this,
+                    triggerHook: 0.5,
+                    duration: "20%"
+                }).setTween(tl)
+                /*.addIndicators()*/
+                .addTo(controller);
+        });
+
+
+        var scene9 = new ScrollMagic.Scene({
+                triggerElement: "#contact",
+                triggerHook: 0.5,
+                duration: "50%"
+            })
+            .setTween(contactTl)
+            /*.addIndicators()*/
+            .addTo(controller);
+
+    } //------scrollmovil
+
+    if ($(window).width() >= 1024) {
+        scroll1024();
+    } else {
+        scrollMovil();
+    }
+
+    var wWidth = $(window).width();
+    var screen = "";
+    if (wWidth < 1024) {
+        screen = "small";
+    } else if (wWidth > 1024) {
+        screen = "big";
+    }
+
+    $(window).resize(function () {
+        imgResize();
+        var currentWidth = $(window).width();
+
+        if (currentWidth < 1024 && screen === "big") {
+            location.reload();
+            screen = "small";
+            console.log("small");
+        } else if (currentWidth >= 1024 && screen === "small") {
+            location.reload();
+            screen = "big";
+            console.log("big");
+        }
+    });
+
+
+    
+    
+   
+   
+    
+    
 });
